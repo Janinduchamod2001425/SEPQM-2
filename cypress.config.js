e762e82336225e581784c00e99d7b4c1599644f0
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-05-02 11:14:55 +0530

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     e2e: {
+        supportFile: 'cypress/support/e2e.js',
         baseUrl: 'http://localhost:5173', // Adjust to your app's URL
         specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
         setupNodeEvents(on, config) {
