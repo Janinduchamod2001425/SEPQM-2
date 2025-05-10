cedbbdb0a008c9f8880bd5ce29842c0b303cf69a
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-05-10 19:42:06 +0530

    Updated

--- cypress/e2e/api/backend-api.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Backend API', () => {
-    it('should return 200 on health check', () => {
-        cy.request('GET', 'http://localhost:3000/api/health')
-            .then((response) => {
-                expect(response.status).to.eq(200)
-            })
-    })
-})
